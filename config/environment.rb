01254555ddfa206408b8a731345752b0a52404ff
Nchebest <Nchebest1@gmail.com>
2018-11-16 14:44:57 +0000

    added ActionMailer

--- config/environment.rb
+++ config/environment.rb
@@ -1,15 +1,16 @@
 # Load the Rails application.
-require_relative 'application'
+require File.expand_path('../application', __FILE__)
+
 
 # Initialize the Rails application.
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
+  :address => 'smtp.sendgrid.net',
+  :port => '587',
+  :authentication => :plain,
   :user_name => ENV['SENDGRID_USERNAME'],
   :password => ENV['SENDGRID_PASSWORD'],
   :domain => 'heroku.com',
-  :address => 'smtp.sendgrid.net',
-  :port => 587,
-  :authentication => :plain,
-  :enable_starttls_auto => true
+  :enable_startstls_auto => true
 }
