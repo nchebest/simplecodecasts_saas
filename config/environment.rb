a2a4aa4a64007007ddd21dff8c7149041b32b0cb
Nchebest <Nchebest1@gmail.com>
2018-11-16 15:21:00 +0000

    Added Action Mailer

--- config/environment.rb
+++ config/environment.rb
@@ -1,17 +1,15 @@
 # Load the Rails application.
-require File.expand_path('../application', __FILE__)
-
+require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-  :address => 'smtp.sendgrid.net',
-  :port => '587',
+  :port           => ENV['MAILGUN_SMTP_PORT'],
+  :address        => ENV['MAILGUN_SMTP_SERVER'],
+  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+  :domain         => 'aqueous-thicket-84243.herokuapp.com',
   :authentication => :plain,
-  :user_name => ENV['SENDGRID_USERNAME'],
-  :password => ENV['SENDGRID_PASSWORD'],
-  :domain => 'aqueous-thicket-84243.herokuapp.com',
-  :enable_startstls_auto => true
 }
 ActionMailer::Base.delivery_method = :smtp
