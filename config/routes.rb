e2dea512e2816b077725e861d5332042f8356fed
Nchebest <Nchebest1@gmail.com>
2018-12-22 23:01:03 +0000

    Updated route file for devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers: { registrations: 'users/registrations' }
      resources :contacts, only: :create
     get 'contact-us', to: 'contacts#new', as: 'new_contact'
     
